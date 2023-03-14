fedf236f1f9a1b8b7f55c37342799b6e962590e5
GUnzner <grit.unzner@googlemail.com>
2023-03-14 17:49:29 +0100

    navbar and sidebars

--- client/src/pages/dashboard/SharedLayout.js
+++ client/src/pages/dashboard/SharedLayout.js
@@ -1,14 +1,20 @@
 import { Outlet, Link } from "react-router-dom";
-import Wrapper from "../../assets/wrappers/RegisterPage";
+import Wrapper from "../../assets/wrappers/SharedLayout";
+import { Navbar, BigSidebar, SmallSidebar } from "../../components";
 
 const SharedLayout = () => {
   return (
     <Wrapper>
-      <nav>
-        <Link to="add-ticket">Add ticket</Link>
-        <Link to="all-tickets">All tickets</Link>
-      </nav>
-      <Outlet />
+      <main className="dashboard">
+        <SmallSidebar />
+        <BigSidebar />
+        <div>
+          <Navbar />
+          <div className="dashboard-page">
+            <Outlet />
+          </div>
+        </div>
+      </main>
     </Wrapper>
   );
 };
